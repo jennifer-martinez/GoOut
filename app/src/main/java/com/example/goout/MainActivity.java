92ac99ac01c5bf5d5364ac51552531fdeda02536
Kelsy <kelsyjohanna_29@hotmail.com>
2019-07-30 23:50:07 -0600

    En esta parte creamos una vista de navegación inferior con fragmentos:
    1.- Primero creamos un android resourde directory con el nombre "menu"
    2.- Segundo un menu resource file con el nombre "bottom_navigation"
    3.-Tercero en el  bottom_navigation.xml creamos el menu con sus respectivos iconos
    4.- Cuarto en activity_main se creo el disñeo del menu

--- app/src/main/java/com/example/goout/MainActivity.java
+++ app/src/main/java/com/example/goout/MainActivity.java
@@ -1,14 +1,85 @@
 package com.example.goout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.goout.Fragment.GouOutFragment;
+import com.example.goout.Fragment.HomeFragment;
+import com.example.goout.Fragment.NotificationFragment;
+import com.example.goout.Fragment.ProfileFragment;
+import com.example.goout.Fragment.SearchFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView  bottomNavigationView;
+    Fragment selectedFragment = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                    switch (menuItem.getItemId()){
+
+                        case R.id.nav_home:
+
+                            selectedFragment = new HomeFragment();
+
+                        break;
+
+                        case R.id.nav_search:
+
+                            selectedFragment = new SearchFragment();
+
+                            break;
+
+                        case R.id.nav_explore:
+
+                            selectedFragment = new GouOutFragment();
+
+                            break;
+
+
+                        case R.id.nav_fav:
+
+                            selectedFragment = new NotificationFragment();
+                            break;
+
+                        case R.id.nav_person:
+
+                            SharedPreferences.Editor editor = getSharedPreferences("PREFS", MODE_PRIVATE).edit();
+                            editor.putString("profield", FirebaseAuth.getInstance().getCurrentUser().getUid());
+                            editor.apply();
+                            selectedFragment = new ProfileFragment();
+
+                            break;
+                    }
+
+                    if(selectedFragment != null){
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                    }
+
+                    return true;
+                }
+            };
 }
+
