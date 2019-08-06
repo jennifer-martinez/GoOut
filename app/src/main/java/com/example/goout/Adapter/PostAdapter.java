4af1beba1cf302cc48413218f157fb50a1961159
jenn <jennifer.martinez1@ujcv.edu.hn>
2019-08-06 07:49:21 -0600

    Seccion de perfil y seccion para subir foto

---
+++ app/src/main/java/com/example/goout/Adapter/PostAdapter.java
@@ -0,0 +1,18 @@
+package com.example.goout.Adapter;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class PostAdapter {
+
+
+    public class ViewHolder extends RecyclerView{
+
+
+        public ViewHolder(@NonNull Context context) {
+            super(context);
+        }
+    }
+}
